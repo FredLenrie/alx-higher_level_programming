179404a00f09b33d6f5e7d399b7b62f7a7e53ca2
FredLenrie <fredlenrieb17@gmail.com>
2024-02-16 04:13:41 -0800

    city relationship updated

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Script that deletes all State objects with a name containing the letter a
-from the database hbtn_0e_6_usa"""
+"""Script that prints all City objects from the database hbtn_0e_14_usa"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from model_city import City
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name.like('%a%'))
-    for state in states:
-        session.delete(state)
-    session.commit()
+    rows = session.query(City, State).filter(City.state_id == State.id)\
+        .order_by(City.id).all()
+    for city, state in rows:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
